23accca5a659261d7c39b67cfb71bf24f9ff3e74
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-01-01 20:26:38 -0300

    Implementando controller para testar manualmente o processamento de feeds.

---
+++ code/MattCanello.NewsFeed.RssReader/Controllers/FeedController.cs
@@ -0,0 +1,24 @@
+﻿using MattCanello.NewsFeed.RssReader.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MattCanello.NewsFeed.RssReader.Controllers
+{
+    [Route("api/feed")]
+    [ApiController]
+    public class FeedController : ControllerBase
+    {
+        private readonly IRssService _rssService;
+
+        public FeedController(IRssService rssService)
+        {
+            _rssService = rssService;
+        }
+
+        [HttpPost("process/{feedId}")]
+        public async Task<IActionResult> Process(string feedId, CancellationToken cancellationToken)
+        {
+            await _rssService.ProcessFeedAsync(feedId, cancellationToken);
+            return NoContent();
+        }
+    }
+}
