1cd84670f456bbd5140ed81d11e70706f8eb1899
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-01-01 19:53:43 -0300

    Implementando testes para RssClient.

---
+++ code/MattCanello.NewsFeed.RssReader.Tests/Mocks/MockedRssHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Text;
+
+namespace MattCanello.NewsFeed.RssReader.Tests.Mocks
+{
+    [ExcludeFromCodeCoverage]
+    internal sealed class MockedRssHandler : DelegatingHandler
+    {
+        private readonly string? _rss;
+        private readonly string? _etag;
+        private readonly DateTimeOffset? _lastPublishDate;
+
+        public MockedRssHandler(string? rss = null, string? etag = null, DateTimeOffset? lastPublishDate = null)
+        {
+            _rss = rss;
+            _etag = etag;
+            _lastPublishDate = lastPublishDate;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HttpContent? content = null;
+
+            if (!string.IsNullOrEmpty(_rss))
+                content = new StringContent(_rss, Encoding.UTF8, "text/xml");
+
+            var statusCode = content is null ? HttpStatusCode.NoContent : HttpStatusCode.OK;
+
+            var isDateUpdated = request.Headers.IfModifiedSince != null && _lastPublishDate <= request.Headers.IfModifiedSince;
+            var isETagUpdated = request.Headers.IfNoneMatch != null && request.Headers.IfNoneMatch.Any() && _etag == request.Headers.IfNoneMatch.First().ToString();
+
+            if (isDateUpdated || isETagUpdated)
+                statusCode = HttpStatusCode.NotModified;
+
+            var response = new HttpResponseMessage(statusCode)
+            {
+                Content = content
+            };
+
+            if (content is null)
+                response.Content = null;
+
+            if (!string.IsNullOrEmpty(_etag))
+                response.Headers.Add("etag", _etag);
+
+            if (_lastPublishDate.HasValue)
+                response.Headers.Date = _lastPublishDate.Value;
+
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ code/MattCanello.NewsFeed.RssReader.Tests/UnitTests/Services/RssClientTests.cs
@@ -0,0 +1,87 @@
+﻿using AutoFixture.Xunit2;
+using MattCanello.NewsFeed.RssReader.Messages;
+using MattCanello.NewsFeed.RssReader.Services;
+using MattCanello.NewsFeed.RssReader.Tests.Mocks;
+using MattCanello.NewsFeed.RssReader.Tests.Properties;
+
+namespace MattCanello.NewsFeed.RssReader.Tests.UnitTests.Services
+{
+    public sealed class RssClientTests
+    {
+        private const string ETag = "W/\"hash963960047320543834b\"";
+        private static readonly DateTimeOffset LastModifiedDate = DateTimeOffset.Parse("Mon, 01 Jan 2024 15:28:01 GMT");
+
+        [Fact]
+        public async Task ReadAsync_WithoutETagAndLastModified_ShouldReturnExpected()
+        {
+            using var httpClient = new HttpClient(new MockedRssHandler(Resources.sample_rss_the_guardian_uk, ETag, LastModifiedDate));
+            var client = new RssClient(httpClient);
+
+            var request = new ReadRssRequestMessage(new Uri("https://www.theguardian.com/uk/rss"));
+            var response = await client.ReadAsync(request);
+
+            Assert.NotNull(response);
+            Assert.NotNull(response.Feed);
+            Assert.False(response.IsNotModified);
+
+            Assert.Equal(ETag, response.ETag);
+            Assert.Equal(LastModifiedDate, response.ResponseDate);
+        }
+
+        [Fact]
+        public async Task ReadAsync_WithSameETag_ShouldReturnIsNotModifed()
+        {
+            using var httpClient = new HttpClient(new MockedRssHandler(Resources.sample_rss_the_guardian_uk, ETag, LastModifiedDate));
+            var client = new RssClient(httpClient);
+
+            var request = new ReadRssRequestMessage(new Uri("https://www.theguardian.com/uk/rss"), ETag);
+            var response = await client.ReadAsync(request);
+
+            Assert.NotNull(response);
+            Assert.Null(response.Feed);
+            Assert.True(response.IsNotModified);
+        }
+
+        [Fact]
+        public async Task ReadAsync_WithSameDate_ShouldReturnIsNotModifed()
+        {
+            using var httpClient = new HttpClient(new MockedRssHandler(Resources.sample_rss_the_guardian_uk, ETag, LastModifiedDate));
+            var client = new RssClient(httpClient);
+
+            var request = new ReadRssRequestMessage(new Uri("https://www.theguardian.com/uk/rss"), lastModifiedDate: LastModifiedDate);
+            var response = await client.ReadAsync(request);
+
+            Assert.NotNull(response);
+            Assert.Null(response.Feed);
+            Assert.True(response.IsNotModified);
+        }
+
+        [Fact]
+        public async Task ReadAsync_WithPastDate_ShouldReturnIsNotModifed()
+        {
+            using var httpClient = new HttpClient(new MockedRssHandler(Resources.sample_rss_the_guardian_uk, ETag, LastModifiedDate));
+            var client = new RssClient(httpClient);
+
+            var request = new ReadRssRequestMessage(new Uri("https://www.theguardian.com/uk/rss"), lastModifiedDate: LastModifiedDate.AddSeconds(-1));
+            var response = await client.ReadAsync(request);
+
+            Assert.NotNull(response);
+            Assert.NotNull(response.Feed);
+            Assert.False(response.IsNotModified);
+        }
+
+        [Fact]
+        public async Task ReadAsync_WithDifferentETag_ShouldReturnIsNotModifed()
+        {
+            using var httpClient = new HttpClient(new MockedRssHandler(Resources.sample_rss_the_guardian_uk, ETag, LastModifiedDate));
+            var client = new RssClient(httpClient);
+
+            var request = new ReadRssRequestMessage(new Uri("https://www.theguardian.com/uk/rss"), "W/\"cce6342964cfd77749fc9f0d277c45d965f09892\"");
+            var response = await client.ReadAsync(request);
+
+            Assert.NotNull(response);
+            Assert.NotNull(response.Feed);
+            Assert.False(response.IsNotModified);
+        }
+    }
+}
