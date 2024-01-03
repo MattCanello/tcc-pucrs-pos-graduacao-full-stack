c628ece57a6dfd8f49321c59bbb796d6cbde304a
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-01-03 14:10:41 -0300

    Implementando testes para EntryService.

---
+++ code/MattCanello.NewsFeed.RssReader.Tests/Mocks/InMemoryEntryPublisher.cs
@@ -0,0 +1,19 @@
+﻿using MattCanello.NewsFeed.RssReader.Interfaces;
+using MattCanello.NewsFeed.RssReader.Models;
+using System.Collections.Concurrent;
+
+namespace MattCanello.NewsFeed.RssReader.Tests.Mocks
+{
+    internal sealed class InMemoryEntryPublisher : IEntryPublisher
+    {
+        private readonly ConcurrentBag<Entry> _publishedEntries = new ConcurrentBag<Entry>();
+
+        public IEnumerable<Entry> PublishedEntries => _publishedEntries;
+
+        public Task PublishNewEntryAsync(Entry entry, CancellationToken cancellationToken = default)
+        {
+            _publishedEntries.Add(entry);
+            return Task.CompletedTask;
+        }
+    }
+}
