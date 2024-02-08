062367665974e88fc56317ad26e61c4517e586b2
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-02-08 20:41:52 -0300

    Implementando testes para CronPublishApp.

---
+++ code/MattCanello.NewsFeed.CronApi.Tests/Fakes/FakeFeedRepository.cs
@@ -0,0 +1,42 @@
+﻿using MattCanello.NewsFeed.CronApi.Domain.Interfaces;
+using MattCanello.NewsFeed.CronApi.Domain.Models;
+
+namespace MattCanello.NewsFeed.CronApi.Tests.Fakes
+{
+    public sealed class FakeFeedRepository : IFeedRepository
+    {
+        private readonly IDictionary<byte, IDictionary<string, Feed>> _feeds;
+        public FakeFeedRepository(IDictionary<byte, IDictionary<string, Feed>> feeds)
+        {
+            _feeds = feeds;
+        }
+
+        public Task<Feed?> GetFeedAsync(byte slot, string feedId, CancellationToken cancellationToken = default)
+        {
+            if (!_feeds.TryGetValue(slot, out var feeds))
+                return Task.FromResult<Feed?>(null);
+
+            if (!feeds.TryGetValue(feedId, out var feed))
+                return Task.FromResult<Feed?>(null);
+
+            return Task.FromResult<Feed?>(feed);
+        }
+
+        public Task<IReadOnlySet<string>> GetFeedIdsAsync(byte slot, CancellationToken cancellationToken = default)
+        {
+            if (!_feeds.TryGetValue(slot, out var feeds))
+                return Task.FromResult<IReadOnlySet<string>>(new HashSet<string>(capacity: 0));
+
+            return Task.FromResult<IReadOnlySet<string>>(new HashSet<string>(feeds.Keys, StringComparer.OrdinalIgnoreCase));
+        }
+
+        public Task PlaceFeedAsync(byte slot, Feed feed, CancellationToken cancellationToken = default)
+        {
+            if (!_feeds.ContainsKey(slot))
+                _feeds[slot] = new Dictionary<string, Feed>(capacity: 1);
+
+            _feeds[slot].Add(feed.FeedId, feed);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ code/MattCanello.NewsFeed.CronApi.Tests/UnitTests/Applications/CronPublishAppTests.cs
@@ -0,0 +1,59 @@
+﻿using AutoFixture.Xunit2;
+using MattCanello.NewsFeed.CronApi.Domain.Applications;
+using MattCanello.NewsFeed.CronApi.Domain.Models;
+using MattCanello.NewsFeed.CronApi.Tests.Fakes;
+
+namespace MattCanello.NewsFeed.CronApi.Tests.UnitTests.Applications
+{
+    public class CronPublishAppTests
+    {
+        [Fact]
+        public async Task PublishSlotAsync_GivenNoFeedIds_ShouldReturnZeroAsync()
+        {
+            const byte slot = 0;
+            var feedRepository = new FakeFeedRepository(new Dictionary<byte, IDictionary<string, Feed>>(capacity: 0));
+
+            var app = new CronPublishApp(feedRepository, new FakeCronFeedEnqueuer());
+
+            var result = await app.PublishSlotAsync(slot);
+            Assert.Equal(0, result);
+        }
+
+        [Theory, AutoData]
+        public async Task PublishSlotAsync_GivenOneFeedId_OnCorrectSlot_ShouldReturnOne(Feed feed)
+        {
+            const byte slot = 0;
+            var feedRepository = new FakeFeedRepository(new Dictionary<byte, IDictionary<string, Feed>>(capacity: 1)
+            {
+                {
+                    slot,
+                    new Dictionary<string, Feed>() { { feed.FeedId, feed } }
+                }
+            });
+
+            var app = new CronPublishApp(feedRepository, new FakeCronFeedEnqueuer());
+
+            var result = await app.PublishSlotAsync(slot);
+            Assert.Equal(1, result);
+        }
+
+        [Theory, AutoData]
+        public async Task PublishSlotAsync_GivenOneFeedId_OnDifferentSlot_ShouldReturnZero(Feed feed)
+        {
+            const byte slotToPublish = 0;
+            const byte feedSlotNumber = 1;
+            var feedRepository = new FakeFeedRepository(new Dictionary<byte, IDictionary<string, Feed>>(capacity: 1)
+            {
+                {
+                    feedSlotNumber,
+                    new Dictionary<string, Feed>() { { feed.FeedId, feed } }
+                }
+            });
+
+            var app = new CronPublishApp(feedRepository, new FakeCronFeedEnqueuer());
+
+            var result = await app.PublishSlotAsync(slotToPublish);
+            Assert.Equal(0, result);
+        }
+    }
+}
