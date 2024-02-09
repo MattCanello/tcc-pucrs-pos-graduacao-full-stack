f81134451b4624d6bb8ae66497cc48b36730dfce
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-02-08 21:25:10 -0300

    Implementando enfileiramento de comando de processamento RSS com Dapr

--- code/MattCanello.NewsFeed.CronApi/Infrastructure/Enqueuers/DaprCronFeedEnqueuer.cs
+++ code/MattCanello.NewsFeed.CronApi/Infrastructure/Enqueuers/DaprCronFeedEnqueuer.cs
@@ -1,25 +1,48 @@
-﻿using Dapr.Client;
+﻿using CloudNative.CloudEvents;
+using Dapr.Client;
 using MattCanello.NewsFeed.CronApi.Domain.Interfaces;
+using MattCanello.NewsFeed.CronApi.Infrastructure.Interfaces;
 
 namespace MattCanello.NewsFeed.CronApi.Infrastructure.Enqueuers
 {
     public sealed class DaprCronFeedEnqueuer : ICronFeedEnqueuer
     {
         private readonly DaprClient _daprClient;
+        private readonly ICloudEventFactory _cloudEventFactory;
+        private readonly CloudEventFormatter _cloudEventFormatter;
         const string BindingName = "rsspublishcommands";
 
-        public DaprCronFeedEnqueuer(DaprClient daprClient)
+        public DaprCronFeedEnqueuer(DaprClient daprClient, ICloudEventFactory cloudEventFactory, CloudEventFormatter cloudEventFormatter)
         {
             _daprClient = daprClient;
+            _cloudEventFactory = cloudEventFactory;
+            _cloudEventFormatter = cloudEventFormatter;
         }
 
-        public Task EnqueueFeedToProcessAsync(string feedId, CancellationToken cancellationToken = default)
+        public async Task EnqueueFeedToProcessAsync(string feedId, CancellationToken cancellationToken = default)
         {
             ArgumentNullException.ThrowIfNull(feedId);
 
-            // TODO: implementar
+            var cloudEvent = _cloudEventFactory.CreateProcessRssEvent(feedId);
 
-            return Task.CompletedTask;
+            var cloudEventData = _cloudEventFormatter.EncodeStructuredModeMessage(cloudEvent, out _);
+
+            var bindingRequest = CreateBindingRequest(feedId, cloudEventData);
+
+            await _daprClient.InvokeBindingAsync(bindingRequest, cancellationToken);
+        }
+
+        public static BindingRequest CreateBindingRequest(string feedId, ReadOnlyMemory<byte> data)
+        {
+            var bindingRequest = new BindingRequest(BindingName, operation: "create")
+            {
+                Data = data
+            };
+
+            bindingRequest.Metadata["partitionKey"] = feedId;
+            bindingRequest.Metadata["PartitionKey"] = feedId;
+
+            return bindingRequest;
         }
     }
 }
