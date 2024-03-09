159f8c614c0156efd1849eb7273f2c11300178ef
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-03-08 21:12:04 -0300

    Adicionando testes para PublishRssEntriesResponse

---
+++ code/MattCanello.NewsFeed.RssReader.Tests/UnitTests/Responses/PublishRssEntriesResponseTests.cs
@@ -0,0 +1,26 @@
+﻿using AutoFixture.Xunit2;
+using MattCanello.NewsFeed.RssReader.Domain.Responses;
+
+namespace MattCanello.NewsFeed.RssReader.Tests.UnitTests.Responses
+{
+    public class PublishRssEntriesResponseTests
+    {
+        [Fact]
+        public void Constructor_GivenInvalidPublishedCount_ShouldThrowException()
+        {
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => new PublishRssEntriesResponse(-1, null));
+
+            Assert.Equal(-1, exception.ActualValue);
+            Assert.Equal("publishedCount", exception.ParamName);
+        }
+
+        [Theory, AutoData]
+        public void Constructor_GivenValidInput_ShouldPreserveData(int publishedCount, DateTimeOffset mostRecentPublishDate)
+        {
+            var response = new PublishRssEntriesResponse(publishedCount, mostRecentPublishDate);
+
+            Assert.Equal(publishedCount, response.PublishedCount);
+            Assert.Equal(mostRecentPublishDate, response.MostRecentPublishDate);
+        }
+    }
+}
