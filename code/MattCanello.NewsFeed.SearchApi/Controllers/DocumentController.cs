0c8a663918fc9d954b0e51d8009a44a7bbf30d7f
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-03-09 20:34:35 -0300

    Unificando controllers de Index e Document (Search API)

--- code/MattCanello.NewsFeed.SearchApi/Controllers/DocumentController.cs
+++ code/MattCanello.NewsFeed.SearchApi/Controllers/DocumentController.cs
@@ -1,4 +1,5 @@
-﻿using MattCanello.NewsFeed.SearchApi.Domain.Interfaces;
+﻿using MattCanello.NewsFeed.SearchApi.Domain.Commands;
+using MattCanello.NewsFeed.SearchApi.Domain.Interfaces;
 using MattCanello.NewsFeed.SearchApi.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
@@ -8,11 +9,28 @@ namespace MattCanello.NewsFeed.SearchApi.Controllers
     [ApiController]
     public class DocumentController : ControllerBase
     {
+        private readonly IIndexApp _indexApp;
         private readonly IEntryRepository _entryRepository;
 
-        public DocumentController(IEntryRepository entryRepository)
+        public DocumentController(IIndexApp indexApp, IEntryRepository entryRepository)
         {
+            _indexApp = indexApp;
             _entryRepository = entryRepository;
+        }
+
+        [HttpPost("index")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Document))]
+        public async Task<IActionResult> Index([FromBody, Required] IndexEntryCommand command, CancellationToken cancellationToken = default)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var id = await _indexApp.IndexAsync(command, cancellationToken);
+
+            var doc = new Document(id, command.FeedId!, command.Entry!);
+
+            return CreatedAtAction("GetById", "Document", new { feedId = command.FeedId, id }, doc);
         }
 
         [HttpGet("feed/{feedId}/document/{id}")]
