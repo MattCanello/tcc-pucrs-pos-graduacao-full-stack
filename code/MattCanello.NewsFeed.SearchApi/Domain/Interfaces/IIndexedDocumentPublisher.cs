25d773e558e9243f045bb638450bb9d5135f450a
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-03-20 19:26:04 -0300

    Implementando evento de notificação de documento indexado

---
+++ code/MattCanello.NewsFeed.SearchApi/Domain/Interfaces/IIndexedDocumentPublisher.cs
@@ -0,0 +1,9 @@
+﻿using MattCanello.NewsFeed.SearchApi.Domain.Models;
+
+namespace MattCanello.NewsFeed.SearchApi.Domain.Interfaces
+{
+    public interface IIndexedDocumentPublisher
+    {
+        Task PublishAsync(Document document, CancellationToken cancellationToken = default);
+    }
+}
