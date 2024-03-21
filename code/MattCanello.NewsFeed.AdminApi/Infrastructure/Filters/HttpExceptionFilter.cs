bd29f0e88158f6e95afcb3f11ae3ddd47388d965
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-03-21 19:56:24 -0300

    Configurações iniciais do projeto de Admin

---
+++ code/MattCanello.NewsFeed.AdminApi/Infrastructure/Filters/HttpExceptionFilter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MattCanello.NewsFeed.AdminApi.Infrastructure.Filters
+{
+    public sealed class HttpExceptionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+        }
+    }
+}
--- code/MattCanello.NewsFeed.AdminApi/Program.cs
+++ code/MattCanello.NewsFeed.AdminApi/Program.cs
@@ -1,33 +1,64 @@
+using MattCanello.NewsFeed.AdminApi.Infrastructure.Filters;
+using MattCanello.NewsFeed.Cross.Dapr.Extensions;
+using MattCanello.NewsFeed.Cross.Telemetry.Extensions;
+using MattCanello.NewsFeed.Cross.Telemetry.Filters;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
 namespace MattCanello.NewsFeed.AdminApi
 {
-    public class Program
+    [ExcludeFromCodeCoverage]
+    static class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddDefaultControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddDapr();
+            builder.Services.AddAppServices();
+            builder.Services.ConfigureHealthChecks();
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
+                app.MapGet("/", () => Results.LocalRedirect("/swagger"));
             }
 
             app.UseAuthorization();
 
-
             app.MapControllers();
 
+            app.MapHealthChecks("/app/health");
+
             app.Run();
+        }
+
+        private static void AddAppServices(this IServiceCollection services)
+        {
+        }
+
+        private static void AddDefaultControllers(this IServiceCollection services)
+        {
+            services.AddControllers(options =>
+            {
+                options.RespectBrowserAcceptHeader = true;
+                options.OutputFormatters.RemoveType<StringOutputFormatter>();
+
+                options.Filters.Add<HttpExceptionFilter>();
+                options.Filters.Add<ActionLoggingFilter>();
+            })
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+            });
         }
     }
 }
