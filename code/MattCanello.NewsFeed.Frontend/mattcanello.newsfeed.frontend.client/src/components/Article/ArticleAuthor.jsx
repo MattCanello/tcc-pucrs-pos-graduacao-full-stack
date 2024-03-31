2fa94767e1c66ef12124fa95cafa66e322b491e2
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-03-31 14:14:24 -0300

    Ajustes de frontend

--- code/MattCanello.NewsFeed.Frontend/mattcanello.newsfeed.frontend.client/src/components/Article/ArticleAuthor.jsx
+++ code/MattCanello.NewsFeed.Frontend/mattcanello.newsfeed.frontend.client/src/components/Article/ArticleAuthor.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import '../../style/ArticleAuthor.css';
 
 function ArticleAuthor({ authors }) {
-    return (
-        <strong>{authors}</strong>
-    );
+    function renderAuthors() {
+        return (authors)
+            ? <strong>{authors}</strong>
+            : null;
+    }
+
+    return renderAuthors();
 }
 
 export default ArticleAuthor;
