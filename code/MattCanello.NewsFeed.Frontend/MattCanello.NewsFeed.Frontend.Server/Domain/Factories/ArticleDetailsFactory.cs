c86f13f2f4d3927dba0d695d4a1217bc0f99e062
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-03-31 02:09:13 -0300

    Mais ajustes de tags HTML nas descrições dos artigos

--- code/MattCanello.NewsFeed.Frontend/MattCanello.NewsFeed.Frontend.Server/Domain/Factories/ArticleDetailsFactory.cs
+++ code/MattCanello.NewsFeed.Frontend/MattCanello.NewsFeed.Frontend.Server/Domain/Factories/ArticleDetailsFactory.cs
@@ -1,11 +1,15 @@
 ﻿using MattCanello.NewsFeed.Frontend.Server.Domain.Interfaces;
 using MattCanello.NewsFeed.Frontend.Server.Domain.Models;
 using MattCanello.NewsFeed.Frontend.Server.Infrastructure.HTML;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace MattCanello.NewsFeed.Frontend.Server.Domain.Factories
 {
     public sealed class ArticleDetailsFactory : IArticleDetailsFactory
     {
+        private static readonly Regex ContinueReadingPattern = new Regex(@"^continue reading|^leia mais|^see all", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
         public ArticleDetails? FromDescription(string? description)
         {
             if (string.IsNullOrEmpty(description))
@@ -13,7 +17,10 @@ namespace MattCanello.NewsFeed.Frontend.Server.Domain.Factories
 
             var lines = description
                 .StripHtmlTags()
-                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                .Select(line => WebUtility.HtmlDecode(line))
+                .Where(line => !IsContinueReading(line))
+                .ToArray();
 
             if (lines.Length == 0)
                 return null;
@@ -24,5 +31,8 @@ namespace MattCanello.NewsFeed.Frontend.Server.Domain.Factories
                 Lines = lines.Skip(1).ToArray()
             };
         }
+
+        public bool IsContinueReading(string line)
+            => ContinueReadingPattern.IsMatch(line);
     }
 }
--- code/MattCanello.NewsFeed.Frontend/MattCanello.NewsFeed.Frontend.Server/Infrastructure/HTML/HtmlHelpers.cs
+++ code/MattCanello.NewsFeed.Frontend/MattCanello.NewsFeed.Frontend.Server/Infrastructure/HTML/HtmlHelpers.cs
@@ -4,9 +4,10 @@ namespace MattCanello.NewsFeed.Frontend.Server.Infrastructure.HTML
 {
     public static class HtmlHelpers
     {
-        static readonly Regex EndPAndEndLI = new Regex(@"<p>|<ul>|<li>|<\/p>|<\/li>|<\/ul>", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        static readonly Regex TagToBreakLinePattern = new Regex(@"<\/?p>|<\/?ul>|<\/?li>", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
         static readonly Regex EverythingBetweenQuotesPattern = new Regex(@"\w+=([""'])(?:(?=(\\?))\2.)*?\1", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
         static readonly Regex AnchorPattern = new Regex(@"<a *>|<\/a>", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        static readonly Regex TagToSupressPattern = new Regex(@"<\/?\w+>", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
 
         public static string StripHtmlTags(this string html)
         {
@@ -17,7 +18,9 @@ namespace MattCanello.NewsFeed.Frontend.Server.Infrastructure.HTML
 
             html = AnchorPattern.Replace(html, string.Empty);
 
-            html = EndPAndEndLI.Replace(html, Environment.NewLine);
+            html = TagToBreakLinePattern.Replace(html, Environment.NewLine);
+
+            html = TagToSupressPattern.Replace(html, string.Empty);
 
             return html;
         }
