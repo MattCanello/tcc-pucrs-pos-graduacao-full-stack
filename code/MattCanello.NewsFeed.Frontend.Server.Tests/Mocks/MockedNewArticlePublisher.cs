08e86db6c165dce6c800fe1d63908aba7ecb447a
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-04-04 23:10:47 -0300

    Mais implementações de testes (frontend)

---
+++ code/MattCanello.NewsFeed.Frontend.Server.Tests/Mocks/MockedNewArticlePublisher.cs
@@ -0,0 +1,19 @@
+﻿using MattCanello.NewsFeed.Frontend.Server.Domain.Interfaces;
+using MattCanello.NewsFeed.Frontend.Server.Domain.Models;
+
+namespace MattCanello.NewsFeed.Frontend.Server.Tests.Mocks
+{
+    sealed class MockedNewArticlePublisher : INewArticlePublisher
+    {
+        private readonly List<Article> _publishedArticles = new List<Article>();
+
+        public IReadOnlyList<Article> PublishedArticles => _publishedArticles;
+
+        public Task ReportNewArticleAsync(Article article, CancellationToken cancellationToken = default)
+        {
+            _publishedArticles.Add(article);
+
+            return Task.CompletedTask;
+        }
+    }
+}
