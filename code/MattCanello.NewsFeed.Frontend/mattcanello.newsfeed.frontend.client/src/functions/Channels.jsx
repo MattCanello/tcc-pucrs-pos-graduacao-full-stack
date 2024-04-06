5c43311962c8f255fc730166173733ee11a534cf
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-04-06 00:57:11 -0300

    Ajustando URL de consumo da listagem de canais pelo React

--- code/MattCanello.NewsFeed.Frontend/mattcanello.newsfeed.frontend.client/src/functions/Channels.jsx
+++ code/MattCanello.NewsFeed.Frontend/mattcanello.newsfeed.frontend.client/src/functions/Channels.jsx
@@ -3,3 +3,9 @@ export async function getChannelArticles({ params }) {
     const data = await response.json();
     return data;
 }
+
+export async function getChannelList() {
+    const response = await fetch(`${import.meta.env.VITE_FRONTEND_SERVER_BASE_ADDRESS}/channels`);
+    const data = await response.json();
+    return data;
+}
