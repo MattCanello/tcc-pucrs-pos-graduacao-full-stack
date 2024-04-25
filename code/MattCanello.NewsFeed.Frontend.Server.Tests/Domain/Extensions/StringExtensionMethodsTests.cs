0467903ab0039ca79d4b7e6c5b79ca5fbb6dd9ce
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-04-25 18:01:25 -0300

    Implementando eliminação da última linha da descrição quando a mesma não estiver completa

---
+++ code/MattCanello.NewsFeed.Frontend.Server.Tests/Domain/Extensions/StringExtensionMethodsTests.cs
@@ -0,0 +1,57 @@
+﻿using MattCanello.NewsFeed.Frontend.Server.Domain.Extensions;
+
+namespace MattCanello.NewsFeed.Frontend.Server.Tests.Domain.Extensions
+{
+    public class StringExtensionMethodsTests
+    {
+        [Fact]
+        public void TrimEllipseLastLine_GivenNullContent_ShouldReturnNull()
+        {
+            var trimmed = StringExtensionMethods.TrimEllipseLastLine(null!);
+
+            Assert.Null(trimmed);
+        }
+
+        [Fact]
+        public void TrimEllipseLastLine_GivenEmptyContent_ShouldReturnStringEmpty()
+        {
+            var trimmed = StringExtensionMethods.TrimEllipseLastLine(string.Empty);
+
+            Assert.NotNull(trimmed);
+            Assert.Empty(trimmed);
+        }
+
+        [Fact]
+        public void TrimEllipseLastLine_GivenRegularLine_ShouldReturnInputString()
+        {
+            var trimmed = StringExtensionMethods.TrimEllipseLastLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+
+            Assert.Equal("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", trimmed);
+        }
+
+        [Fact]
+        public void TrimEllipseLastLine_GivenEllipseLine_ShouldReturnTrimmedString()
+        {
+            var trimmed = StringExtensionMethods.TrimEllipseLastLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit egestas dui id ornare arcu odio ut sem nulla...");
+
+            Assert.Equal("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", trimmed);
+        }
+
+        [Fact]
+        public void TrimEllipseLastLine_GivenOnlyEllipseLine_ShouldReturnStringEmpty()
+        {
+            var trimmed = StringExtensionMethods.TrimEllipseLastLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...");
+
+            Assert.NotNull(trimmed);
+            Assert.Empty(trimmed);
+        }
+
+        [Fact]
+        public void TrimEllipseLastLine_GivenTwiceEllipseLine_ShouldReturnTrimmedString()
+        {
+            var trimmed = StringExtensionMethods.TrimEllipseLastLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit egestas dui id ornare arcu odio ut sem nulla... Et netus et malesuada fames. Nulla malesuada pellentesque elit eget gravida...");
+
+            Assert.Equal("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit egestas dui id ornare arcu odio ut sem nulla... Et netus et malesuada fames.", trimmed);
+        }
+    }
+}
---
+++ code/MattCanello.NewsFeed.Frontend/MattCanello.NewsFeed.Frontend.Server/Domain/Extensions/StringExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MattCanello.NewsFeed.Frontend.Server.Domain.Extensions
+{
+    public static class StringExtensionMethods
+    {
+        private static readonly Regex LastLinePattern = new Regex(@"(?<lastLine>([^\.!?]*[\.!?])\s*\.\.)$", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+        public static string TrimEllipseLastLine(this string content)
+        {
+            if (string.IsNullOrEmpty(content))
+                return content;
+
+            content = content.Trim();
+           
+            var lastLineMatches = LastLinePattern.Match(content);
+
+            if (!lastLineMatches.Success)
+                return content;
+
+            var lastLineMatch = lastLineMatches.Groups["lastLine"];
+            return content.Replace(lastLineMatch.Value, "").Trim();
+        }
+    }
+}
