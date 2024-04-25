0dc0667e912f341c31a5865ab730dc9cfcc1978f
Mateus Canello Ottoni <mateuscottoni@outlook.com>
2024-04-25 18:12:54 -0300

    Implementando removeção de tags <div> do sumário de artigos

--- code/MattCanello.NewsFeed.Frontend/MattCanello.NewsFeed.Frontend.Server/Infrastructure/HTML/HtmlHelpers.cs
+++ code/MattCanello.NewsFeed.Frontend/MattCanello.NewsFeed.Frontend.Server/Infrastructure/HTML/HtmlHelpers.cs
@@ -8,11 +8,14 @@ namespace MattCanello.NewsFeed.Frontend.Server.Infrastructure.HTML
         static readonly Regex EverythingBetweenQuotesPattern = new Regex(@"\w+=([""'])(?:(?=(\\?))\2.)*?\1", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
         static readonly Regex AnchorPattern = new Regex(@"<a *>|<\/a>", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
         static readonly Regex TagToSupressPattern = new Regex(@"<\/?\w+>|<\w+ *\/?>", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+        static readonly Regex DivTagPattern = new Regex(@"(?<div><div(.*?)<\/div>)", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
 
         public static string StripHtmlTags(this string html)
         {
             if (string.IsNullOrEmpty(html))
                 return html;
+
+            html = DivTagPattern.Replace(html, " ");
 
             html = EverythingBetweenQuotesPattern.Replace(html, string.Empty);
 
